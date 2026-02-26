8456c0a5779bd7ab2d68f3703eab9fe1dd31ed64
havanthinhnb20-ai <havanthinhnb20@gmail.com>
2026-02-26 21:36:32 +0700

    laptrinh7

--- bai2.c
+++ bai2.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Hà Văn Thịnh]
+ * MSSV:      [PS49131]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 //  BÀI 2: XÂY DỰNG CHƯƠNG TRÌNH ĐĂNG NHẬP BẰNG USERNAME VÀ PASSWORD 
@@ -10,3 +10,28 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char username[50], password[50];
+    char correctUsername[] = "admin";
+    char correctPassword[] = "123456";
+
+    printf("Nhap username: ");
+    scanf("%s", username);
+    printf("Nhap password: ");
+    scanf("%s", password);
+
+    if (strcmp(username, correctUsername) == 0 && strcmp(password, correctPassword) == 0)
+    {
+        printf("Dang nhap thanh cong!\n");
+    }
+    else
+    {
+        printf("Dang nhap that bai!\n");
+    }
+
+    return 0;
+}
--- bai3.c
+++ bai3.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Hà Văn Thịnh]
+ * MSSV:      [PS49131]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH SẮP XẾP CHUỖI THEO CHỮ CÁI 
@@ -10,3 +10,41 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char chuoi[5][100];
+    char temp[100];
+
+    printf("Nhap 5 chuoi:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Chuoi thu %d: ", i + 1);
+        fgets(chuoi[i], sizeof(chuoi[i]), stdin);
+        chuoi[i][strcspn(chuoi[i], "\n")] = '\0'; // Loại bỏ ký tự newline
+    }
+
+    // Sắp xếp các chuỗi theo thứ tự bảng chữ cái
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = i + 1; j < 5; j++)
+        {
+            if (strcmp(chuoi[i], chuoi[j]) > 0)
+            {
+                strcpy(temp, chuoi[i]);
+                strcpy(chuoi[i], chuoi[j]);
+                strcpy(chuoi[j], temp);
+            }
+        }
+    }
+
+    printf("\nCac chuoi sau khi sap xep:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", chuoi[i]);
+    }
+
+    return 0;
+}
